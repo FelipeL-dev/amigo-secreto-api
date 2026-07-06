8cbe62f728f4c84e094915884d1e3564899d5284
Felipe Lopes <felpscokinho@gmail.com>
2026-07-06 20:38:42 -0300

    fix: fix: made enviarResultadoSorteio asynchronous so realizarSorteio works independently of email sending

--- src/main/java/com/projeto/amigo/secreto/service/EmailService.java
+++ src/main/java/com/projeto/amigo/secreto/service/EmailService.java
@@ -3,6 +3,7 @@ package com.projeto.amigo.secreto.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Async
     public void enviarResultadoSorteio(String emailDestinatario, String nomeSorteador, String nomeSorteado) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(emailDestinatario);
