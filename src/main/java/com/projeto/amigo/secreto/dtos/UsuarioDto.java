2bdf70c8921938d6bdc71332d594f942e9ecc459
Felipe Lopes <felpscokinho@gmail.com>
2026-08-14 21:47:52 -0300

    refactor: add @builder to UsuarioDto

--- src/main/java/com/projeto/amigo/secreto/dtos/UsuarioDto.java
+++ src/main/java/com/projeto/amigo/secreto/dtos/UsuarioDto.java
@@ -1,12 +1,14 @@
 package com.projeto.amigo.secreto.dtos;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UsuarioDto {
     private Long id;
     private String nome;
